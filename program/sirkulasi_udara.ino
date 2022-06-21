053523bd9cf9620ec752e22c0990ad842b14281f
fauziallagan <fauziallagan@gmail.com>
2022-06-21 10:09:40 +0700

    update Blynk IoT terbaru

--- program/sirkulasi_udara.ino
+++ program/sirkulasi_udara.ino
@@ -1,48 +1,50 @@
-// ** Import Library ** //
-#include "DHT.h"                // DHT Sensor
-#include <ESP8266WiFi.h>        //EPS8266WIFI module library
-#include <BlynkSimpleEsp8266.h> // Blynk library
+#include "DHT.h"
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
 
-// ** Connection to Blynk ** //
-// IMPORTANT! PLEASE EDIT THIS VARIABLE BELOW
-char auth[] = "YOUR AUTHENTICATION BLYNK"; // Auth blynk Account
-// char authLocal[] = "YOUR AUTHENTICATION BLYNK FOR LOCAL SERVER"; // auth local server  * Do not Use! *
-char ssid[] = "YOUR SSID WIFI";        // SSID WIFI
-char pass[] = "YOUR PASSWORD WIFI"; // Password WIFI
+// Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
+// See the Device Info tab, or Template settings
+#define BLYNK_TEMPLATE_ID "" // use tample id
+#define BLYNK_DEVICE_NAME "" // use device name
+#define BLYNK_AUTH_TOKEN ""  // use auth token
+// Comment this out to disable prints and save space
+#define BLYNK_PRINT Serial
 
+char auth[] = BLYNK_AUTH_TOKEN;
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = ""; // change ssid
+char pass[] = ""; // change password
+
+// sensors
 // ** Declaration Variabel Sensor ** //
 DHT dht(D5, DHT22); // DHT instance named dht, D5 and sensor type
-// Global Declaration
-int ldrSensors = A0; // LDR instance named ldrSensors, A0
-int nilai;           // count LDR
 
+// Global Declaratio
+int ldrSensors = A0; // LDR instance named ldrSensors, A0
+int ldr;             // count LDR
 // **  if relay ready, uncomment variabel below  ** //
 #define relay1 D6 // relay module IN 1
 #define relay2 D7 // relay module IN 2
 
 void setup()
 {
-  Serial.begin(115200);          // serial boundrate 115200
-  Blynk.begin(auth, ssid, pass); // Connecting to blynk
-  // Blynk.begin(authLocal, IPAddress(192,168,1,9), 8080); //  blynk local server * Do not Use! *
+  // put your setup code here, to run once:
+  Serial.begin(115200); // serial boundrate 115200
+  Blynk.begin(auth, ssid, pass);
   dht.begin(); // Starting  DHT-22
-  // pinMode(ledTesting, OUTPUT);              // declare LED
-  pinMode(relay1, OUTPUT);
-  pinMode(relay2, OUTPUT);
+  // pinMode(ledTesting, OUTPUT);           // declare LED
+  pinMode(relay1, OUTPUT); // set relay1 for output Kipas
+  pinMode(relay2, OUTPUT); // set relay2 for output Lampu
 }
 
 void loop()
 {
+  // put your main code here, to run repeatedly:
   Blynk.run();
-  dhtSensors(); // call function DHT
-  ldrSensor();  // call function LDR
-  delay(100);
-  decisionSensors(); //  100 milisecond
-
-  // ** Call Function Testing Component ** //
-  // relaySensorTesting();  // use if relay not working!
-  // dhtTesting();          // use if dht working!
-  // ldrTesting();          // use if ldr not working!
+  sensorStarted(); //  1 detik
+  delay(1000);
 }
 
 void dhtSensors()
@@ -59,107 +61,99 @@ void dhtSensors()
 
   // ** example statement
   // Condition DHT
-  // if(hum >= ... && temp >= ...){
-  //   // Code here!
-  // }else if(hum <= ... && temp <= ...){
-  //   // Code Here!
+  // statement :
+  /*
+    jika temp kurang dari 21 derajat celcius maka lampu on dan kipas off
+    jika temp lebih dari 21 atau dibawah 28 maka lampu off kipas on
+  */
+  if (temp <= 21)
+  {
+    // Code here!
+    digitalWrite(relay2, LOW);
+    digitalWrite(relay1, HIGH);
+    Serial.println("Lampu ON & kipas Off");
+  }
+  else if (temp <= 28)
+  {
+    // Code Here!
+    digitalWrite(relay2, HIGH);
+    //     digitalWrite(relay1, LOW);
+    Serial.println("Lampu OFF & kipas On");
+  }
+  else if (temp == 255 || hum == 255)
+  {
+    Serial.println("Warning!. The Device Something Wrong!. Please checking the sensors!");
+    Blynk.notify("Alat Tidak Bekerja");
+  }
+  else
+  {
+    Serial.println("Sensor bekerja");
+  }
+}
 
-  // }else{
-  //   Serial.println("Sensor DHT Not Detected!");
-  //   digitalWrite(relay1, LOW);
-  //   digitalWrite(relay2, LOW);
-  // }
+void relaySensorTesting()
+{
+  // Example Code Relay Here!
+  digitalWrite(relay1, LOW); // Menyalakan relay Kipas
+  Serial.println("LOW");
+  delay(1000);               // Jeda 1 Detik
+  digitalWrite(relay2, LOW); // Menyalakan relay lampu
+  Serial.println("LOW");
 }
 
 void ldrSensor()
 {
   //  Code LDR here!
-  nilai = analogRead(ldrSensors);
-  Blynk.virtualWrite(V2, nilai); // Set Virtual Pin 2 (V2) frequency to  in Blynk app
-  Serial.println(nilai);         // print variable nilai
+  ldr = analogRead(ldrSensors);
+  Blynk.virtualWrite(V2, ldr); // Set Virtual Pin 2 (V2) frequency to  in Blynk app
+  Serial.print("LDR Value : ");
+  Serial.println(ldr); // print variable nilai
 
   // Condition LDR
-  if (nilai >= 500)
+  if (ldr < 100)
   {
     //   over 500
-    digitalWrite(relay1, HIGH);
-    Serial.println("Fan ON");
-  }
-  else if (nilai <= 500)
-  {
-    //    under 500
-    digitalWrite(relay1, LOW);
+    digitalWrite(relay2, HIGH);
     Serial.println("Fan OFF");
   }
-  else
+  else if (ldr > 600)
   {
-    Serial.println("LDR Not Detected!");
-    digitalWrite(relay1, LOW);
+    //    under 500
     digitalWrite(relay2, LOW);
+
+    Serial.println("Fan ON");
   }
 }
 
-void decisionSensors()
+void sensorStarted()
 {
   byte hum = dht.readHumidity();     // temp and  are celsius and humidity readings
   byte temp = dht.readTemperature(); // from DHT/A
 
-  if (hum >= 80 && temp >= 30 || ldrSensors >= 1000)
+  if (temp <= 70 && ldrSensors <= 1000) // true or true = true
   {
-    Blynk.notify("LED ON && Fan ON");
     Serial.println("Alat Bekerja!");
     dhtSensors(); // call function dht
-    ldrSensor();  // call function LDR
-    delay(10000);
-  }
-  if (hum >= 100 || temp >= 100 || ldrSensors >= 1024)
-  {
-    Blynk.notify("Peringatan!. Sensor Tidak Sedang Baik Baik Saja");
-    dhtSensors(); // call function dht
-    ldrSensor();  // call function LDR
+
+    ldrSensor(); // call function LDR
+    Serial.println("\n=======================================================================\n");
     delay(100);
-    if (temp > 100)
-    {
-      Blynk.notify("Masalah pada Sensor DHT bagian temperatur");
-      Serial.println("Masalah pada Sensor DHT bagian temperatur");
-      dhtSensors(); // call function dht
-      ldrSensor();  // call function LDR
-    }
-    else if (hum > 100)
-    {
-      Blynk.notify("Masalah pada Sensor DHT bagian humadity");
-      Serial.println("Masalah pada Sensor DHT bagian humadity");
-      dhtSensors(); // call function dht
-      ldrSensor();  // call function LDR
-    }
-    else if (ldrSensors >= 1024)
-    {
-      Blynk.notify("Masalah pada Sensor LDR");
-      Serial.println("Masalah Pada Sensor LDR");
-      dhtSensors(); // call function dht
-      ldrSensor();  // call function LDR
-    }
   }
   else
   {
-    // Blynk.notify("Device not Working");
+    //  do nothing
   }
+  while (temp == 100)
+  {
+    Serial.println("Peringatan!. Sensor DHT  :");
+    Serial.print(temp);
+  }
+  Blynk.notify("Alat mengalami msaalah!, Sensor Tidak Bekerja");
+  Serial.println("Sensor Tidak bekerja");
 }
 
 // ** TESTING Component ** //
 // ** Use This function For Testing Component ** //
-
-void relaySensorTesting()
-{
-  // Example Code Relay Here!
-  digitalWrite(relay1, HIGH); // Menyalakan relay
-  delay(1000);                // Jeda 1 Detik
-  digitalWrite(relay1, LOW);
-  delay(1000);
-  digitalWrite(relay2, HIGH);
-  delay(1000);
-  digitalWrite(relay2, LOW);
-}
 
 void dhtTesting()
 {
@@ -174,7 +168,7 @@ void dhtTesting()
 
 void ldrTesting()
 {
-  nilai = analogRead(ldrSensors);
-  nilai = nilai / 10;
-  Serial.println(nilai); // print variable nilai
+  ldr = analogRead(ldrSensors);
+  ldr = ldr / 10;
+  Serial.println(ldr); // print variable ldr
 }
